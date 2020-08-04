1964a821e502f8237315016e8de8b6f478b937b4
lambrohan <lambrohan@gmail.com>
2020-08-04 23:10:35 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0d259a5cec88bd54ebf1f600685ccc74'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd03fb735-1d21-4bcf-9ba1-de653726fda3'
+};
